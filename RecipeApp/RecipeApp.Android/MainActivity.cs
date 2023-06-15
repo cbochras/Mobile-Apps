710bf58825d02d710d3769e589f5f99bee008ea5
Cherifa Bochra Soltani <65483776+soltani-bochra@users.noreply.github.com>
2023-06-15 10:25:11 +0300

    Add files via upload

---
+++ RecipeApp/RecipeApp.Android/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.IO;
+using System.Xml.Linq;
+using Android.Content.Res;
+
+namespace ReciepeApp.Droid
+{
+    [Activity(Label = "ReciepeApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        public static XDocument xml { get; set; }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            AssetManager assets = this.Assets;
+            //string conditionName = "";
+            using (StreamReader sr = new StreamReader(assets.Open("Test.XML")))
+            {
+                xml = XDocument.Load(sr);
+            }
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+    }
+}
+
---
+++ RecipeApp/RecipeApp.Android/SQLiteService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ReciepeApp.Droid
+{
+    class SQLiteService
+    {
+        public XDocument GetXML()
+        {
+            return MainActivity.xml;
+        }
+    }
+}
